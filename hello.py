8544fa914a89df9f2b526b4f515434315b561b79
Jordi Beringues <jberingues@jcm-tech.com>
2024-05-16 12:20:47 +0200

    Versió que em permet connectar-me a la RBPI des del VSC i executar codi, però no sé on s'executa.

--- hello.py
+++ hello.py
@@ -1,12 +1,3 @@
-# https://www.digitalocean.com/community/tutorials/how-to-set-up-ssh-keys--2
-# https://www.digitalocean.com/community/tutorials/how-to-use-sshfs-to-mount-remote-file-systems-over-ssh
-# sudo sshfs -o allow_other -o IdentityFile=~/.ssh/id_rsa pi@192.168.0.122:/home/pi /mnt/rpi3plus/
-# sudo umount /mnt/rpi3plus/
-
-
-import ptvsd 
-ptvsd.enable_attach("glovebox", address=('0.0.0.0', 3003))
-ptvsd.wait_for_attach()   #optional​
 
 import time
 time.sleep(0.5)
