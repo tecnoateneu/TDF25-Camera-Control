595cb8f71c375be9edb9d205211c849ee6655c0a
Jordi Beringues <jberingues@jcm-tech.com>
2024-05-16 22:50:47 +0200

    Imatge calibracio acabat però no operatiu

--- src/Calibracio-camera/Imatges-calibracio.py
+++ src/Calibracio-camera/Imatges-calibracio.py
@@ -1,39 +1,56 @@
 import cv2
+from picamera2 import Picamera2, Preview
+import time
 
-# It reads images from the camera and saves them in the folder Eines/Calibracio-camera/Imatges
-# The images are saved with the name caliX.png, where X is a number that increases with each image saved
-# The program stops when the user presses the 'esc' key
-# The user can save an image by pressing the 's' key
+# Llegeix imatges de la càmera de la Raspberry i les guarda a la carpeta Eines/Calibracio-camera/Imatges
+# Les imatges es guarden amb el nom caliX.png, on X és un número que augmenta amb cada imatge guardada
+# El programa s'atura quan l'usuari prem la tecla 'esc'
+# L'usuari pot guardar una imatge prement la tecla 's'
 def LlegeixImatgesCalibracio():
 
-    cap = cv2.VideoCapture('rtsp://admin:TAV1234a@192.168.1.116:554/11')
-
-    # Check if the camera opened successfully
-    if not cap.isOpened():
-        print("LlegeixFotoCamera: Could not open camera.")
-        exit()
-
-    num = 0
+    # Activa càmera
+    try:
+        picam2 = Picamera2()
+        camera_config = picam2.create_still_configuration(main={"size": (4608, 2592)}, lores={"size": (640, 480)}, display="lores")
+        picam2.configure(camera_config)
+        picam2.start()
+    except IOError:
+        print("ActivaCamera: No s'ha pogut obrir o accedir a la càmera.")
+    except ValueError:
+        print("ActivaCamera: S'han proporcionat paràmetres no vàlids a create_still_configuration o configure.")
+    except RuntimeError:
+        print("ActivaCamera: S'ha cridat a start quan la càmera ja està en ús o no està configurada correctament.")
+    except Exception as e:
+        print(f"ActivaCamera: S'ha produït un error inesperat: {str(e)}")
+    time.sleep(2)
     
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            print("LlegeixFotoCamera: Failed to capture frame.")
-        
-        k = cv2.waitKey(5)
-        
-        if k == 27:
-            break
-        elif k == ord('s'):
+    # Bucle que va llegint imatges de la càmera de la Raspberry
+    # Mostra la imatge llegida en una finestra
+    # Si la tecla 's' és premuda, guarda la imatge amb el nom caliX.png
+    # Si la tecla 'esc' és premuda, el programa s'atura
+    num = 0
+    while True:
+        # Llegeix una imatge de la càmera
+        output = picam2.capture_array()                     # Captura la imatge 
+        output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)   # Converteix la imatge a escala de grisos
+  
+        # Mostra la imatge en una finestra
+        cv2.imshow('Imatge de la càmera', output)
+
+        # Espera que l'usuari premi una tecla
+        k = cv2.waitKey(1)
+
+        # Si l'usuari ha premut la tecla 's', guarda la imatge
+        if k == ord('s'):
+            cv2.imwrite('Imatges/' + f'cali{num}.png', output)
             num += 1
-            nomfitxer = 'cali' + str(num) + '.png'
-            if not cv2.imwrite('Eines/Calibracio-camera/Imatges/' + nomfitxer, frame):
-                print('Error al gravar la imatge.')
-            else:
-                print('Imatge gravada com a ' + nomfitxer)
-        
-        cv2.imshow('Imatge de la càmera', frame)
-    cap.release()
+
+        # Si l'usuari ha premut la tecla 'esc', surt del bucle
+        elif k == 27:  # 27 és el codi ASCII per a la tecla 'esc'
+            break
+
+    # Allibera els recursos de la càmera i tanca les finestres d'OpenCV
+    picam2.release()
     cv2.destroyAllWindows()
 
 def main():
