816cf0576a428e025924d524c43dc3b4a1ac5f26
Jordi Beringues <159556249+jberinguesa@users.noreply.github.com>
2024-09-01 18:14:39 +0200

    Bucle que agafa imatges quan s'apreta tecla 's'

--- src/TestCamera.py
+++ src/TestCamera.py
@@ -3,7 +3,7 @@
 # Ensenya la imatge de la càmera i es pot canviar el focus de la càmera
 
 import cv2
-import time  
+import datetime
 
 # Limits del camp de flors
 DOWN_LEFT = (60, 2500)
@@ -11,12 +11,20 @@ DOWN_RIGHT = (4460, 2515)
 UP_LEFT = (10, 160)
 UP_RIGHT = (4470, 130)
 
-CAMERA = 'DAHUA_1_RUBEN'
+#CAMERA = 'DAHUA_1_RUBEN'
+#CAMERA = 'TPTEK'
+CAMERA = 'WEBCAM'
 TEST_VELOCITAT = True
+TEST_IMATGE = False
+TEST_CAMP = False
 
 # Obrir càmera
 if CAMERA == 'DAHUA_1_RUBEN':
     cap = cv2.VideoCapture('rtsp://admin:Holaquetal@192.168.1.160:554/cam/realmonitor?channel=1&subtype=0')
+if CAMERA == 'TPTEK':
+    cap = cv2.VideoCapture('rtsp://admin:TAV1234a@192.168.1.173:554/11')
+if CAMERA == 'WEBCAM':
+    cap = cv2.VideoCapture(0)
 
 # Check if the camera opened successfully
 if not cap.isOpened():
@@ -27,27 +35,64 @@ ret, frame = cap.read()
 
 if TEST_VELOCITAT:
     #print HH:MM:SS:MS
-    current_time_ms = int(time.time() * 1000)
-    print(f"Anem a llegir: {current_time_ms} ms")
     ret, frame = cap.read()
-    current_time_ms = int(time.time() * 1000)
-    print(f"Imatge llegida: {current_time_ms} ms")
-    
-    if not ret:
-        print("LecturaCamera: No s'ha pogut capturar imatge.")
-        
-    output = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   # Converteix la imatge a escala de grisos
-
-    print(f"Imatge passada a grisos: {current_time_ms} ms")
-    
-    cv2.imshow('Imatge', output)
-
+    cv2.imshow('Imatge', frame)
     while True:
         k = cv2.waitKey(1)
         if k == 27:
             break
+        if k == ord('s'):
+            now = datetime.datetime.now()
+            formatted_time = now.strftime("%H:%M:%S:%f")[:-3]
+            print(f"Anem a llegir: {formatted_time}")
+            ret, frame = cap.read()
+            now = datetime.datetime.now()
+            formatted_time = now.strftime("%H:%M:%S:%f")[:-3]
+            print(f"Imatge llegida: {formatted_time}")
+            
+            if not ret:
+                print("LecturaCamera: No s'ha pogut capturar imatge.")
+                
+            output = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   # Converteix la imatge a escala de grisos
 
-else:
+            now = datetime.datetime.now()
+            formatted_time = now.strftime("%H:%M:%S:%f")[:-3]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            formatted_time = now.strftime("%H:%M:%S:%f")[:-3]
+            cv2.putText(output, formatted_time, (10, 50), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            print(f"Imatge passada a grisos: {formatted_time}")
+            
+            cv2.imshow('Imatge', output)
+            # Guardar la imatge
+            if not cv2.imwrite('src/Data/ImatgeCamera.png', output):
+                print('Error al gravar la imatge.')
+            else:
+                print('Imatge gravada')
+
+            
+
+if TEST_IMATGE:
+    # Llegir la imatge fins que es premi la tecla 'esc'
+    while True:
+        ret, frame = cap.read()
+        
+        if not ret:
+            print("LecturaCamera: No s'ha pogut capturar imatge.")
+            
+        output = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   # Converteix la imatge a escala de grisos
+        # Imprimeix l'hora en la foto
+        now = datetime.datetime.now()
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        formatted_time = now.strftime("%H:%M:%S:%f")[:-3]
+        cv2.putText(output, formatted_time, (10, 50), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.imshow('Imatge', output)
+        
+        # Espera que l'usuari premi una tecla
+        k = cv2.waitKey(1)
+        if k == 27:
+            break
+
+if TEST_CAMP:
     while True:
         ret, frame = cap.read()
         
